06d5ce0f92cfc31d9f03a36fef5a771f4978b248
tsingke <tsingke@sdnu.edu.cn>
2022-11-08 11:44:39 +0800

    GO update

--- GO.m
+++ GO.m
@@ -1,16 +1,11 @@
-% -----------------------------------------------
-% Growth Optimizer: A powerful metaheuristic algorithm
-% Version:1.0
-% Corresponding author: Qingke Zhang 
-% Email: tsingke@sdnu.edu.cn,GaooHaoo@hotmail.com
-% -----------------------------------------------
 function [gbestX,gbestfitness,gbesthistory]= GO(popsize,dimension,xmax,xmin,MaxFEs,Func,FuncId)
-FEs=0;
-Fitness=Func;
-x=xmin+(xmax-xmin)*unifrnd(0,1,popsize,dimension);
+%Parameter setting
+FEs=0;P1=5;P2=0.001;P3=0.3;
+%initialization
+x=unifrnd(xmin,xmax,popsize,dimension);
 gbestfitness=inf;
 for i=1:popsize
-    fitness(i)=Fitness(x(i,:)',FuncId);
+    fitness(i)=Func(x(i,:)',FuncId);
     FEs=FEs+1;
     if gbestfitness>fitness(i)
         gbestfitness=fitness(i);
@@ -22,43 +17,43 @@ end
 while 1
     [~, ind]=sort(fitness);
     Best_X=x(ind(1),:);
-%% Learning phase
+    %% Learning phase
     for i=1:popsize
-        Worst_X = x(ind(randi([popsize-4,popsize])),:);
-        Better_X=x(ind(randi([2,5])),:);
+        Worst_X = x(ind(randi([popsize-P1+1,popsize],1)),:);
+        Better_X=x(ind(randi([2,P1],1)),:);
         random=selectID(popsize,i,2);
         L1=random(1);
         L2=random(2);
-        D_value1=(Best_X-Better_X);
-        D_value2=(Best_X-Worst_X);
-        D_value3=(Better_X-Worst_X);
-        D_value4=(x(L1,:)-x(L2,:));
-        Distance1=norm(D_value1);
-        Distance2=norm(D_value2);
-        Distance3=norm(D_value3);
-        Distance4=norm(D_value4);
-        rate=Distance1+Distance2+Distance3+Distance4;
-        LF1=Distance1/rate;
-        LF2=Distance2/rate;
-        LF3=Distance3/rate;
-        LF4=Distance4/rate;
+        Gap1=(Best_X-Better_X);
+        Gap2=(Best_X-Worst_X);
+        Gap3=(Better_X-Worst_X);
+        Gap4=(x(L1,:)-x(L2,:));
+        Distance1=norm(Gap1);
+        Distance2=norm(Gap2);
+        Distance3=norm(Gap3);
+        Distance4=norm(Gap4);
+        SumDistance=Distance1+Distance2+Distance3+Distance4;
+        LF1=Distance1/SumDistance;
+        LF2=Distance2/SumDistance;
+        LF3=Distance3/SumDistance;
+        LF4=Distance4/SumDistance;
         SF=(fitness(i)/max(fitness));
-        Gap1=LF1*SF*D_value1;
-        Gap2=LF2*SF*D_value2;
-        Gap3=LF3*SF*D_value3;
-        Gap4=LF4*SF*D_value4;
-        newx(i,:)=x(i,:)+Gap1+Gap2+Gap3+Gap4;
+        KA1=LF1*SF*Gap1;
+        KA2=LF2*SF*Gap2;
+        KA3=LF3*SF*Gap3;
+        KA4=LF4*SF*Gap4;
+        newx(i,:)=x(i,:)+KA1+KA2+KA3+KA4;
         %Clipping
         newx(i,:)=max(newx(i,:),xmin);
         newx(i,:)=min(newx(i,:),xmax);
-        newfitness=Fitness(newx(i,:)',FuncId);
+        newfitness=Func(newx(i,:)',FuncId);
         FEs=FEs+1;
         %Update
         if fitness(i)>newfitness
             fitness(i)=newfitness;
             x(i,:)=newx(i,:);
         else
-            if rand<0.001&&ind(i)~=1
+            if rand<P2&&ind(i)~=1
                 fitness(i)=newfitness;
                 x(i,:)=newx(i,:);
             end
@@ -73,15 +68,16 @@ while 1
     if FEs>=MaxFEs
         break;
     end
-%% Reflection phase
+    %% Reflection phase
     for i=1:popsize
         newx(i,:)=x(i,:);
         j=1;
         while j<=dimension
-            if rand<0.3
-                R=x(ind(randi(5)),:);
+            if rand<P3
+                R=x(ind(randi(P1)),:);
                 newx(i,j) = x(i,j)+(R(:,j)-x(i,j))*unifrnd(0,1);
-                if rand<(0.01+(0.1-0.01)*(1-FEs/MaxFEs))
+                AF=(0.01+(0.1-0.01)*(1-FEs/MaxFEs));
+                if rand<AF
                     newx(i,j)=xmin+(xmax-xmin)*unifrnd(0,1);
                 end
             end
@@ -90,14 +86,14 @@ while 1
         %Clipping
         newx(i,:)=max(newx(i,:),xmin);
         newx(i,:)=min(newx(i,:),xmax);
-        newfitness=Fitness(newx(i,:)',FuncId);
+        newfitness=Func(newx(i,:)',FuncId);
         FEs=FEs+1;
         %Update
         if fitness(i)>newfitness
             fitness(i)=newfitness;
             x(i,:)=newx(i,:);
         else
-            if rand<0.001&&ind(i)~=1
+            if rand<P2&&ind(i)~=1
                 fitness(i)=newfitness;
                 x(i,:)=newx(i,:);
             end
@@ -112,7 +108,7 @@ while 1
     if FEs>=MaxFEs
         break;
     end
-end   
+end
 
 %Deal with the situation of too little or too much evaluation
 if FEs<MaxFEs
@@ -122,5 +118,19 @@ else
         gbesthistory(MaxFEs+1:end)=[];
     end
 end
+end
 
+function [r]=selectID(popsize,i,k)
+%Generate k random integer values within [1,popsize] that do not include i and do not repeat each other
+if k<= popsize
+    vecc=[1:i-1,i+1:popsize];
+    r=zeros(1,k);
+    for kkk =1:k
+        n = popsize-kkk;
+        t = randi(n,1,1);
+        r(kkk) = vecc(t);
+        vecc(t)=[];
+    end
+end
+end
 
---
+++ main.m
@@ -0,0 +1,30 @@
+% Growth Optimizer: A powerful metaheuristic algorithm for solving different optimization problems
+% Hao Gao 08.2022, School of Information Science and Engineering, Shandong Normal University, Jinan
+% Email:GaooHaoo@hotmail.com
+% Corresponding author: Qingke Zhang
+% Email:tsingke@sdnu.edu.cn
+% Note: 1.The CEC 2017 file has been recompiled to return fitness value errors.
+%       2.This version of GO uses the maximum number of evaluations (MaxFEs) as its termination criterion.
+%       3.Need to load input_data_17 file for shifting, rotating, etc
+%       4.Not using 1E-8 as a condition for finding the global optimal solution
+clc;
+clear;
+close all;
+format short e
+rand('state', sum(100*clock));
+%Parameter setting
+xmax=100;                 %Upper bound
+xmin=-100;                %Lower bound
+popsize=40;               %Population size (Too large value will slow down the convergence speed)
+dimension=10;             %Problem dimension (dimension=10/30/50/100)
+MaxFEs=dimension*10000;   %Maximum number of evaluations MaxFEs=dimension*10000
+Func=@cec17_func;         %Objective function set
+FuncId=[];                %Function number
+
+for FuncId=1:30
+    fprintf("\n*******Current test function No.: F%d*******\n",FuncId);
+    pause(2);
+    [gbestX,gbestfitness,gbesthistory]= GO(popsize,dimension,xmax,xmin,MaxFEs,Func,FuncId);
+    plot(1:MaxFEs,gbesthistory,'Linewidth',3);
+    title(['F',num2str(FuncId),'  Optimization History'])
+end
